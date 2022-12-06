ca967fa2b13d2157e9e62770922aa95d1d608101
Teo <mgttmich@gmail.com>
2022-12-06 20:32:32 +0200

    Html canvas, css canvas, consts, start class Explosion

---
+++ main.js
@@ -0,0 +1,18 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = 500;
+canvas.height = 700;
+
+// ctx.fillStyle = "white";
+// ctx.fillRect(50, 50, 100, 150);
+
+const explosions = [];
+
+class Explosion {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.spriteWidth = 200;
+        this.spriteHeight = 179;
+    }
+}
